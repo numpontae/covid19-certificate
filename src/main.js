0a937de143c00887be2ebf6ca1b444d135112b8f
numpon <numpontae09@gmail.com>
2021-08-11 08:41:14 +0700

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "@/config/axios";
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+Vue.use(BootstrapVue);
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
 
 new Vue({

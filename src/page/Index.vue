65ade1ee3bbb13e7189c9f4309d8af2187cff41d
numpon <numpontae09@gmail.com>
2021-08-17 12:40:06 +0700

    commit

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -18,53 +18,177 @@
       </b-row>
     </b-container>
     <div class="container">
-      <div
-        id="printMe"
-        class="printMe"
-        v-if="isFound == true"
-        style="margin-top: 2rem"
-      >
+      <div id="printMe" class="printMe" v-if="isFound == true">
         <div
-          style="padding-left:10px;padding-right:10px; margin-top:2rem"
+          style="padding-left: 10px; padding-right: 10px"
           v-if="isFound == true"
         >
-          <p align="center">
-            <img
-              alt="Samitivej"
-              src="../assets/SNH.jpg"
-              style="height: 90px"
-              v-if="result.site == 'SNH'"
-            />
-            <img
-              alt="Samitivej"
-              src="../assets/SVH.jpg"
-              style="height: 90px"
-              v-if="result.site == 'SVH'"
-            />
+        <!-- <div class="container" style="padding-right: 0"> -->
+          <table border="0" style="margin-top: 2rem">
+            <tr >
+              <td rowspan="4">
+                <img
+                  alt="Samitivej"
+                  src="../assets/SNH.jpg"
+                  style="height: 90px"
+                  v-if="result.site == 'SNH'"
+                />
+                <img
+                  alt="Samitivej"
+                  src="../assets/SVH.jpg"
+                  style="height: 90px"
+                  v-if="result.site == 'SVH'"
+                />
+              </td>
+              <td>
+                <b>
+                  &nbsp;
+                </b>
+              </td>
+              
+            </tr>
+            <tr>
+              <td style="font-size: 12px; line-height:4px; width:100%;" v-if="result.site == 'SNH'">
+                <b>
+                  133 สุขุมวิท 49 แขวงคลองตันเหนือ เขตวัฒนา กรุงเทพฯ 10110 โทร.
+                  0-2022-2222 แฟ็กซ์: (66) 0-2391-1290
+                </b>
+              </td>
+              <td style="font-size: 12px; line-height:4px; width:100%;" v-if="result.site == 'SVH'">
+                <b>
+                  133 สุขุมวิท 49 แขวงคลองตันเหนือ เขตวัฒนา กรุงเทพฯ 10110 โทร.
+                  0-2022-2222 แฟ็กซ์: (66) 0-2391-1290
+                </b>
+              </td>
+            </tr>
+            <tr style="border-top: 2px solid black;">
+              <td style="font-size: 12px; padding-top:0;line-height:4px; width:100%;padding-bottom:7px" v-if="result.site == 'SNH'">
+                <b>
+                  133 SUKHUMVIT 49, KLONGTAN NUA, VADHANA, BANGKOK 10110,
+                  THAILAND, TEL. 0-2022-2222 FAX: (66) 0-2391-1290
+                </b>
+              </td>
+              <td style="font-size: 12px; padding-top:0;line-height:4px; width:100%;padding-bottom:7px" v-if="result.site == 'SVH'">
+                <b>
+                  133 SUKHUMVIT 49, KLONGTAN NUA, VADHANA, BANGKOK 10110,
+                  THAILAND, TEL. 0-2022-2222 FAX: (66) 0-2391-1290
+                </b>
+              </td>
+            </tr>
+            
+          </table>
+          </div>
+          <div
+            style="padding-left: 10px; padding-right: 10px; margin-top: 2rem"
+            v-if="isFound == true"
+          >
+          </div>
+          <p
+            style="font-size: 25px; margin-top: 2rem; text-align: center"
+            align="center"
+          >
+            <b>Laboratory Report</b>
           </p>
-          <p style="font-size: 25px" align="center"><b>Laboratory Report</b></p>
-          <b-container style="margin-top: 2rem; padding-top: 1rem; border-top: 2px solid black;" >
-            <b-row class="text-left" style="text-algin: left; ">
+          
+          <div
+          style="padding-left: 10px; padding-right: 10px"
+          v-if="isFound == true"
+        >
+          <table border="0" style="margin-top: 1rem; padding-top: 1rem; border-top: 2px solid black;width:100%">
+            <tr style="padding-top: 2rem;">
+              <td style="padding-top: 1rem;" width="20%"><b>Patient Name :</b></td>
+              
+              <td style="padding-top: 1rem;" colspan="2" width="15%">{{ result.patientname }}</td>
+              <td style="padding-top: 1rem;" width="10%"></td>
+              <td style="padding-top: 1rem;" width="18%"><b>Sex : </b>{{ result.sex }}</td>
+              <td style="padding-top: 1rem;" width="18%"><b>Age : </b>{{ result.age }}</td>
+              <td style="padding-top: 1rem;" width="19%"><b>DOB : </b>{{ result.dob }}</td>
+              </tr>
+              <tr style="padding-top: 2rem;">
+              <td style="padding-top: 1rem;" width="20%"><b>Hospital Number :</b></td>
+              
+              <td style="padding-top: 1rem;" colspan="2" width="15%">{{ result.hn }}</td>
+              <td style="padding-top: 1rem;" width="10%"></td>
+              <td style="padding-top: 1rem;" width="18%"><b>Lab Episode :</b></td>
+              <td style="padding-top: 1rem;" width="18%">{{ result.labnumber }}</td>
+              <td style="padding-top: 1rem;" width="19%"></td>
+              </tr>
+              <tr style="padding-top: 2rem;">
+              <td style="padding-top: 1rem;" width="20%"><b>Collected Date/Time :</b></td>
+              
+              <td style="padding-top: 1rem;" colspan="2" width="15%">{{ result.dateOfCollect }}</td>
+              <td style="padding-top: 1rem;" width="10%"></td>
+              <td style="padding-top: 1rem;" width="18%"><b>Doctor :</b></td>
+              <td style="padding-top: 1rem;" colspan="2" width="18%">{{ result.doctor }}</td>
+              <td style="padding-top: 1rem;" width="19%"></td>
+              </tr>
+            </table>
+          <!-- <b-container
+            style="
+              margin-top: 1rem;
+              padding-top: 1rem;
+              border-top: 2px solid black;
+            "
+          >
+            <b-row class="text-left" style="text-algin: left">
               <b-col cols="3"><b>Patient Name :</b></b-col>
               <b-col cols="3">{{ result.patientname }}</b-col>
               <b-col cols="2"><b>Sex: </b>{{ result.sex }}</b-col>
               <b-col cols="2"><b>Age: </b>{{ result.age }}</b-col>
               <b-col cols="2"><b>DOB: </b>{{ result.dob }}</b-col>
             </b-row>
-            <b-row style="margin-top: 1rem;" class="text-left">
+            <b-row style="margin-top: 1rem" class="text-left">
               <b-col cols="3"><b>Hospital Number:</b></b-col>
               <b-col cols="3">{{ result.hn }}</b-col>
               <b-col cols="2"><b>Lab Episode :</b></b-col>
               <b-col cols="2">{{ result.labnumber }}</b-col>
             </b-row>
-            <b-row style="margin-top: 1rem; padding-bottom: 1rem; border-bottom: 2px solid black;" class="text-left">
+            <b-row
+              style="
+                margin-top: 1rem;
+                padding-bottom: 1rem;
+                border-bottom: 2px solid black;
+              "
+              class="text-left"
+            >
               <b-col cols="3"><b>Collected Date/Time :</b></b-col>
               <b-col cols="3">{{ result.dateOfCollect }}</b-col>
               <b-col cols="2"><b>Doctor :</b></b-col>
               <b-col cols="4">{{ result.doctor }}</b-col>
             </b-row>
-          </b-container>
-          <b-container style="margin-top: 3rem" >
+          </b-container> -->
+          <table border="0" style="margin-top: 1rem; padding-top: 1rem; border-top: 2px solid black;width:100%">
+            <tr style="padding-top: 2rem;">
+              <td style="padding-top: 1rem;" colspan="6" width="15%"><b>{{ result.ctts_nme }}</b></td>
+              
+              <td style="padding-top: 1rem;" width="15%"></td>
+              <td style="padding-top: 1rem;" width="15%"></td>
+              <td style="padding-top: 1rem;" width="18%"></td>
+              <td style="padding-top: 1rem;" width="18%"></td>
+              <td style="padding-top: 1rem;" width="19%"></td>
+              </tr>
+              <tr>
+              <td style="padding-top: 1rem;padding-left: 2rem" width="25%"><b>Method :</b></td>
+              
+              <td style="padding-top: 1rem;margin-left: 2rem" colspan="2" width="75%">{{ result.method }}</td>
+              </tr>
+              <tr>
+              <td style="padding-top: 1rem;padding-left: 2rem" width="25%"><b>Specimen :</b></td>
+              
+              <td style="padding-top: 1rem;margin-left: 2rem" colspan="2" width="75%">{{ result.specimen }}</td>
+              </tr>
+              <tr>
+              <td style="padding-top: 1rem;padding-left: 2rem" width="25%"><b>SARS-Cov-2 RNA :</b></td>
+              
+              <td style="padding-top: 1rem;margin-left: 2rem" colspan="2" width="75%">{{ result.sars }}</td>
+              </tr>
+              <tr>
+              <td style="padding-top: 4rem;padding-left: 2rem" width="25%"><b>Limit of detection :</b></td>
+              
+              <td style="padding-top: 4rem;margin-left: 2rem" colspan="2" width="75%">{{ result.limit }}</td>
+              </tr>
+            </table>
+          <!-- <b-container style="margin-top: 3rem">
             <b-row class="text-left" style="text-algin: left">
               <b-col cols="12"
                 ><b>{{ result.ctts_nme }}</b></b-col
@@ -92,182 +216,81 @@
               >
               <b-col cols="4">{{ result.limit }}</b-col>
             </b-row>
-          </b-container>
-          <!-- <div class="table-responsive" style="margin-top:0rem">
-            <table class="table borderless">
-              <tbody> -->
-          <!-- <tr>
-                  <td stlye="text-algin:left"><b>Patient Name :</b></td>
-                  <td>{{ result.patientname }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td><b>Sex :</b></td>
-                  <td>{{ result.sex }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td><b>Age :</b></td>
-                  <td>{{ result.age }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td><b>DOB :</b></td>
-                  <td>{{ result.dob }}</td>
-                  <td></td>
-                  <td></td>
-                </tr> -->
-          <!-- <tr>
-                  <td><b>Hospital Number :</b></td>
-                  <td>{{ result.hn }}</td>
-                  <td></td>
-                  <td></td>
-                </tr> -->
-          <!-- <tr>
-                  <td><b>Collected Date/Time :</b></td>
-                  <td>{{ result.dateOfCollect }}</td>
-                  <td></td>
-                  <td></td>
-                </tr> -->
-          <!-- <tr>
-                  <td><b>Doctor :</b></td>
-                  <td>{{ result.doctor }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td><b>Lab Episode :</b></td>
-                  <td>{{ result.labnumber }}</td>
-                  <td></td>
-                  <td></td>
-                </tr> -->
-          <!-- </tbody>
-            </table> -->
-          <!-- </div> -->
-        </div>
-        <!-- <div
-          class="col-xs-12 col-md-10 offset-md-1 pt-5"
-          v-if="isFound == true"
-          style="margin-top: 3rem"
-        >
-          <div class="table-responsive">
-            <table class="table borderless">
-              <tbody>
-                <tr>
-                  <td stlye="text-algin:left">
-                    <b>{{ result.ctts_nme }}</b>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Method :</td>
-                  <td>{{ result.method }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Specimen :</td>
-                  <td>{{ result.specimen }}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>SARS-Cov-2 RNA :</td>
-                  <td>{{ result.sars }}</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Limit of detection :</td>
-                  <td>{{ result.limit }}</td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+          </b-container> -->
           </div>
-        </div> -->
+        <!-- </div> -->
         <div
-          style="padding-left:10px;padding-right:10px"
+          style="padding-left: 10px; padding-right: 10px"
           v-if="isFound == true"
         >
-        <b-container style="margin-top: 3rem; margin-bottom: 3rem">
-          <b-row class="text-left" style="text-algin: left">
-            <b-col cols="9"
-              ></b-col
-            >
-            <b-col cols="3"
-              ><vue-qrcode
-                      v-if="qrValue != null && qrValue != '' && isFound == true"
-                      :value="qrValue"
-                      class="border border-dark"
-                      style="height: 200px; width: 200px"
-                    />
-                    </b-col
-            >
-          </b-row>
-          <b-row style="margin-top: 2rem; padding-top: 1rem; border-top: 2px solid black;" class="text-left">
-            <b-col cols="12"
-              >Reported by : {{ result.reported }}</b-col
-            >
-          </b-row>
-          <b-row style="margin-top: 1rem; padding-bottom: 1rem; border-bottom: 2px solid black;" class="text-left">
-            <b-col cols="12"
-              >Authorised by : {{ result.authorised }}</b-col
-            >
-          </b-row>
-          
-        </b-container>
-        </div>
-        <!-- <div
-          class="col-xs-12 col-md-10 offset-md-1 pt-5"
-          v-if="isFound == true"
-          style="margin-top: 3rem"
-        >
-          <div class="table-responsive">
-            <table class="table borderless">
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td>
-                    
-                  </td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
+          <table border="0" style="margin-top: 30rem; margin-bottom: 3rem;width:100%">
+            <tr style="padding-top: 2rem;">
+              <td width="80%"></td>
+              
+              <td style="padding-top: 1rem;" width="20%"><vue-qrcode
+                  v-if="qrValue != null && qrValue != '' && isFound == true"
+                  :value="qrValue"
+                  style="height: 200px; width: 200px"
+                /></td>
+              </tr>
+              
             </table>
-          </div>
-        </div> -->
-        <!-- <div
-          style="
-            text-align: right;
-            margin-top: 3rem;
-            padding-top: 1rem;
-            padding-right: 10%;
-          "
-        >
-          <vue-qrcode
-            v-if="qrValue != null && qrValue != '' && isFound == true"
-            :value="qrValue"
-            class="border border-dark"
-            style="height: 200px; width: 200px"
-          />
-        </div> -->
+            <table border="0" style="margin-top: 1rem; margin-bottom: 3rem;width:100%">
+            <tr style="padding-top: 2rem;">
+              <tr style="
+                margin-top: 2rem;
+                padding-top: 1rem;
+                border-top: 2px solid black;
+              ">
+              <td style="padding-top: 1rem;" width="100%"><b>Reported by : </b>{{ result.reported }}</td>
+              </tr>
+              <tr style="
+                margin-top: 1rem;
+                padding-bottom: 1rem;
+                border-bottom: 2px solid black;
+              ">
+              <td style="padding-top: 1rem;padding-bottom: 1rem;" width="100%"><b>Authorised by : </b>{{ result.authorised }}</td>
+      
+              </tr>
+            </table>
+          <!-- <b-container style="margin-top: 30rem; margin-bottom: 3rem">
+            <b-row class="text-left" style="text-algin: left">
+              <b-col cols="9"></b-col>
+              <b-col cols="3"
+                ><vue-qrcode
+                  v-if="qrValue != null && qrValue != '' && isFound == true"
+                  :value="qrValue"
+                  style="height: 200px; width: 200px"
+                />
+              </b-col>
+            </b-row>
+            <b-row
+              style="
+                margin-top: 2rem;
+                padding-top: 1rem;
+                border-top: 2px solid black;
+              "
+              class="text-left"
+            >
+              <b-col cols="12"><b>Reported by : </b>{{ result.reported }}</b-col>
+            </b-row>
+            <b-row
+              style="
+                margin-top: 1rem;
+                padding-bottom: 1rem;
+                border-bottom: 2px solid black;
+              "
+              class="text-left"
+            >
+              <b-col cols="12"><b>Authorised by : </b>{{ result.authorised }}</b-col>
+            </b-row>
+          </b-container> -->
+        </div>
       </div>
-      <div style="margin-top: 3rem; margin-bottom: 3rem">
+      <div style="margin-top: 3rem; margin-bottom: 3rem; text-align: center">
         <b-button
+          class="btn-lg"
+          style="width: 15%"
           v-if="isFound == true"
           @click="print()"
           variant="outline-primary"
@@ -316,7 +339,7 @@ const options = {
   specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
   styles: [
     "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
-    "https://unpkg.com/kidlat-css/css/kidlat.css",
+    // "https://unpkg.com/kidlat-css/css/kidlat.css",
   ],
 };
 Vue.use(VueHtmlToPaper, options);
@@ -358,6 +381,7 @@ export default {
         authorised: null,
         reported: null,
       },
+      data: []
     };
   },
   methods: {
@@ -367,7 +391,7 @@ export default {
       let labData = await this.$http.get(
         `/api/v1/patient/getpatientlabcovid19?labnumber=${this.search.labnumber}`
       );
-
+      this.data = labData.data
       labData.data.map((d) => {
         if (d.CTTC_Cde == "M0003" && d.CTTC_Des == "Specimen") {
           this.result.specimen = d.LabResult;
@@ -415,9 +439,8 @@ export default {
           },
           "Ar3b1Op"
         );
-        this.qrValue = `http://192.168.1.104:8080/Result?token=` + token;
-        // this.qrValue = `http://phr.samitivejhospitals.com/?token=` + token;
-        console.log(this.qrValue);
+        // this.qrValue = `http://192.168.1.104:8080/Result?token=` + token;
+        this.qrValue = `http://phr.samitivejhospitals.com/?token=` + token;
         this.isFound = true;
       }
     },
@@ -426,6 +449,9 @@ export default {
             refs function generatePdf()
         */
     print() {
+      let body = this.data
+      // console.log(this.data)
+            this.$http.post(`https://phr.samitivejhospitals.com:3000/api/v1/patient/postpatientlabcovid19`, body)
       this.$htmlToPaper("printMe");
       // this.$refs.html2Pdf.generatePdf()
     },
@@ -462,6 +488,6 @@ a {
   text-align: left;
 }
 .printMe {
-  border: 1px solid #007065;
+  border: 1px solid hsl(174, 100%, 22%);
 }
 </style>

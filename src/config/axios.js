713f26c13760ce3151d2e15b9c485931aaaa2588
numpon <numpontae09@gmail.com>
2021-08-18 16:19:33 +0700

    commit

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// axios.defaults.baseURL = "http://10.104.10.85:30063";
-axios.defaults.baseURL = "http://192.168.1.104:30020";
+axios.defaults.baseURL = "http://10.104.10.85:30063";
+// axios.defaults.baseURL = "http://192.168.1.104:30020";
 axios.defaults.headers.common = {
   "Content-Type": "application/json"
 };
